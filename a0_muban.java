25892d0a73f1aa7685d4c74969f077b4f9589c61
xiangyaoshuihui <1559440223@qq.com>
2023-03-07 18:19:47 +0800

    first

---
+++ a0_muban.java
@@ -0,0 +1,6 @@
+
+public class a0_muban {
+    public static void main(String[] args){
+        System.out.println("helloworld");
+    }
+}
---
+++ sum_of_two_1.java
@@ -0,0 +1,5 @@
+public class sum_of_two_1 {
+    public static void main(String[] args){
+
+    }
+}
