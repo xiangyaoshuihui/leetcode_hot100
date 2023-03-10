2b02a97ebfd74b9e457cd503d199cd4c60620441
xiangyaoshuihui <1559440223@qq.com>
2023-03-10 17:07:03 +0800

    easy_70

--- easy_70.java
+++ easy_70.java
@@ -1,3 +1,19 @@
+
+
 public class easy_70 {
-    
+    public static void main(String[] args){
+        System.out.println(climbStairs(3));
+    }
+
+    public static  int climbStairs(int n){
+        int []dp=new int[50];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-2]+dp[i-1];
+        }
+        
+
+        return dp[n];
+    }
 }
